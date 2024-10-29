0ac6a5575ff87452156ec7ab4d2e5d90492444b3
Srisankar2001 <srisank-se20028@stu.kln.ac.lk>
2024-10-29 23:42:19 +0530

    Initialize

---
+++ src/main/java/com/example/multi_lang/MultiLangApplication.java
@@ -0,0 +1,13 @@
+package com.example.multi_lang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiLangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multi_lang/MultiLangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_lang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
